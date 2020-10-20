0b2a97a5de902a3a36f051d24a844b59348fef9e
Valérie Mauduit <valerie.mauduit@pricefx.eu>
2020-10-20 10:14:38 +0200

    Add all in once, since I wasn't clean enough until now

--- comparaison-vocabulaire.py
+++ comparaison-vocabulaire.py
@@ -1,9 +1,11 @@
 import os
 
+from vocabulary_utils import regroupement_statistiques, mots_specifiques
+
 def main():
     titres = os.listdir('books')
-    # statistiques = regroupement_statistiques(titres)
-    # mots_specifiques(statistiques)
+    statistiques = regroupement_statistiques(titres)
+    mots_specifiques(statistiques)
 
 
 if __name__ == "__main__":
---
+++ proximite.py
@@ -0,0 +1,1 @@
+# TODO: pour un mot donné, le top des mots qui sont situés à moins de n mots de lui
